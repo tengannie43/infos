a85d8a659e221105bf3e633a2fc990a2b10e4af5
tengannie43 <tengannie@gmail.com>
2025-06-10 17:34:09 +0800

    Create App.jsx

---
+++ public/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>食食在 問卷首頁</h1>
+      <p><a href="/gender">開始填寫問卷</a></p>
+    </div>
+  );
+}
